a6be87f832d53bc1db0aa8f074904ea8af7b4b50
Adrien DEGAND <2024697310@lacatholille.fr>
2026-02-03 12:21:58 +0100

    add generator

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -7,19 +7,19 @@ public class Generator : MonoBehaviour
     public GameObject ball;
     public GameObject pin;
 
-    void OnCollisionEnter(Collision otherObj)
+    public void generateBowling()
     {
-        Instantiate(ball, new Vector3(0.09, 0.3, 0.45), Quaternion.identity);
+        Instantiate(ball, new Vector3(0.09f, 0.3f, 0.45f), Quaternion.identity);
 
-        Instantiate(pin, new Vector3(0, 0.5, 0.8), Quaternion.identity);
-        Instantiate(pin, new Vector3(0.188, 0.5, 1.048), Quaternion.identity);
-        Instantiate(pin, new Vector3(-0.22, 0.5, 1.051), Quaternion.identity);
-        Instantiate(pin, new Vector3(0, 0.5, 1.3), Quaternion.identity);
-        Instantiate(pin, new Vector3(0.4, 0.5, 1.3), Quaternion.identity);
-        Instantiate(pin, new Vector3(-0.4, 0.5, 1.3), Quaternion.identity);
-        Instantiate(pin, new Vector3(-0.2, 0.5, 1.5), Quaternion.identity);
-        Instantiate(pin, new Vector3(0.2, 0.5, 1.5), Quaternion.identity);
-        Instantiate(pin, new Vector3(0.6, 0.5, 1.5), Quaternion.identity);
-        Instantiate(pin, new Vector3(-0.7, 0.5, 1.5), Quaternion.identity);
+        Instantiate(pin, new Vector3(0f, 0.5f, 0.8f), Quaternion.identity);
+        Instantiate(pin, new Vector3(0.188f, 0.5f, 1.048f), Quaternion.identity);
+        Instantiate(pin, new Vector3(-0.22f, 0.5f, 1.051f), Quaternion.identity);
+        Instantiate(pin, new Vector3(0f, 0.5f, 1.3f), Quaternion.identity);
+        Instantiate(pin, new Vector3(0.4f, 0.5f, 1.3f), Quaternion.identity);
+        Instantiate(pin, new Vector3(-0.4f, 0.5f, 1.3f), Quaternion.identity);
+        Instantiate(pin, new Vector3(-0.2f, 0.5f, 1.5f), Quaternion.identity);
+        Instantiate(pin, new Vector3(0.2f, 0.5f, 1.5f), Quaternion.identity);
+        Instantiate(pin, new Vector3(0.6f, 0.5f, 1.5f), Quaternion.identity);
+        Instantiate(pin, new Vector3(-0.7f, 0.5f, 1.5f), Quaternion.identity);
     }
 }
